2ffe8bfbd35b122a7b20cd09e250a8d9a159a49a
Domer078 <khomotsomabala05@gmail.com>
2025-11-09 17:45:22 +0200

    Added User Experience with blink animation

--- src/index.js
+++ src/index.js
@@ -12,10 +12,14 @@ function Generate(event) {
 
   let apikey = "63340413at53cc6c6ba7a81a11oc3f05";
   let context =
-    "You are an expert when it comes to poems, please write a short four lines of a poem. Sign at the end of the poem 'SheCodes AI ' and should have a <strong> element";
-  let prompt = `Generate a english poem about ${poemInputElement.value}`;
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a English poem about${poemInputElement.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
   let poemInputElement = document.querySelector("poem-input");
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${poemInputElement.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
