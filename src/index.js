8b7e4c1b07274a050c861c5c0277b2697a52e0e3
Domer078 <khomotsomabala05@gmail.com>
2025-11-21 21:05:22 +0200

    Linked my JS file to my HTML file

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ function displayPoem(response) {
     strings: response.data.answer,
     autoStart: true,
     delay: 10,
-    cursor: none,
+    cursor: "",
   });
 }
 
 function Generate(event) {
   event.preventDefault();
+  let poemInputElement = document.querySelector("#poem-input");
 
   let apikey = "63340413at53cc6c6ba7a81a11oc3f05";
   let context =
     "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a English poem about${poemInputElement.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
-  let poemInputElement = document.querySelector("poem-input");
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
